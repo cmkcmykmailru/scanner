a720b23c818beae4ee96f87b427d69aefc9acf01
cmkcmykmailru <45236493+cmkcmykmailru@users.noreply.github.com>
2021-01-25 22:56:55 +0300

    Add TargetSupport

--- tests/ScannerTest/Driver/File/ComponentTest.php
+++ tests/ScannerTest/Driver/File/ComponentTest.php
@@ -4,6 +4,9 @@ namespace ScannerTest\Driver\File;
 
 use PHPUnit\Framework\TestCase;
 use Scanner\Driver\File\File;
+use Scanner\Driver\File\System\Read\ReadSupport;
+use Scanner\Driver\File\System\Read\Strategy\YamlReadStrategy;
+use Scanner\Driver\Support\Target\TargetSupport;
 
 class ComponentTest extends TestCase
 {
@@ -23,5 +26,29 @@ class ComponentTest extends TestCase
         $file = new File('/var/www/scanner/src/Scanner/Event/LeafListener.php');
         $file->addSupport(DummySupport::create($file));
         $file->get100('dummy');
+    }
+
+    public function testSupportMethodsCalled()
+    {
+        $file = new File(__DIR__ . '/test.yml');
+        $readSupport = ReadSupport::create($file);
+        $readSupport->setReadStrategy(new YamlReadStrategy());
+
+        $targetSupport = TargetSupport::create($file);
+        $handler = new DummyTargetHandle();
+        $targetSupport->setHandle($handler);
+
+        $file->addSupport($readSupport);
+        $file->addSupport($targetSupport);
+
+        $yml = $file->read();
+        ob_start();
+        $file->target();
+        $out = ob_get_clean();
+
+        self::assertIsArray($yml);
+        self::assertArrayHasKey('version', $yml);
+        self::assertEquals('45', $yml['version']);
+        self::assertEquals('target', $out);
     }
 }
---
+++ tests/ScannerTest/Driver/File/DummyTargetHandle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ScannerTest\Driver\File;
+
+use Scanner\Driver\Component;
+use Scanner\Driver\Support\Target\TargetHandle;
+
+class DummyTargetHandle implements TargetHandle
+{
+
+    public function handle(Component $component): void
+    {
+        echo 'target';
+    }
+}
